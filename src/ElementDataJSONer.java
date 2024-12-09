d6bd93f5cb89245fe5a006c4223c78fa098de6c0
Leul Alemayehu <leulalemayehu435@gmail.com>
2024-12-09 17:08:16 +0300

    Small changes to JSONer

--- src/ElementDataJSONer.java
+++ src/ElementDataJSONer.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
