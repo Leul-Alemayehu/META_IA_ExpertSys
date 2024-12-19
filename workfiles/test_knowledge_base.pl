ce7017bdca6229aea8ac3b0bdda1c7884468273b
Leul Alemayehu <leulalemayehu435@gmail.com>
2024-12-19 20:31:38 +0300

    Finalized the knowledge base

--- workfiles/test_knowledge_base.pl
+++ workfiles/test_knowledge_base.pl
@@ -1,5 +1,5 @@
 % Properties for: Hydrogen
-atomic_properties(hydrogen, atomic_number(1), atomic_symbol(h), atomic_mass(1.00)).
+atomic_properties(hydrogen, atomic_number(1), atomic_symbol(h), atomic_mass(1.01)).
 oxidation_states(hydrogen, [-1, +1]).
 physical_properties(hydrogen, [state(gas), color([colorless]), density(0.00008988), melting_point(-259.34), boiling_point(-252.87), thermal_conductivity(low_(insulator)), electrical_conductivity(low_(insulator)), crystal_structure(hcp)]).
 chemical_properties(hydrogen, [toxicity(no), reactivity(highly_reactive), oxide_acidity_basicity(amphoteric), reaction_with_oxygen(forms_oxides), reaction_with_water(no_reaction), reaction_with_acids(no_reaction), magnetism(diamagnetic)]).
@@ -10,12 +10,138 @@ atomic_properties(helium, atomic_number(2), atomic_symbol(he), atomic_mass(4.00)
 oxidation_states(helium, [0]).
 physical_properties(helium, [state(gas), color([colorless]), density(0.0001785), melting_point(-272.2), boiling_point(-268.93), thermal_conductivity(low_(insulator)), electrical_conductivity(low_(insulator)), crystal_structure(hcp)]).
 chemical_properties(helium, [toxicity(no), reactivity(inert), oxide_acidity_basicity(not_applicable), reaction_with_oxygen(does_not_react), reaction_with_water(no_reaction), reaction_with_acids(no_reaction), magnetism(diamagnetic)]).
-miscellaneous_properties(helium, [flame_test_color(n/a), isotope_count(9), allotrope_count(3), corrosion_resistance(low_general_reactivity), refractive_index(1), hardness(n/a), radioactivity(no), group(2), period(1), ionization_energy(24.587), electron_configuration('1s2'), electronegativity(4.16)]).
+miscellaneous_properties(helium, [flame_test_color(n/a), isotope_count(9), allotrope_count(3), corrosion_resistance(low_general_reactivity), refractive_index(1), hardness(n/a), radioactivity(no), group(18), period(1), ionization_energy(24.587), electron_configuration('1s2'), electronegativity(4.16)]).
 
 % Properties for: Lithium
 atomic_properties(lithium, atomic_number(3), atomic_symbol(li), atomic_mass(7.00)).
 oxidation_states(lithium, [+1]).
 physical_properties(lithium, [state(solid), color([silvery_white]), density(0.534), melting_point(180.5), boiling_point(1342), thermal_conductivity(high), electrical_conductivity(high), crystal_structure(bcc)]).
 chemical_properties(lithium, [toxicity(yes), reactivity(highly_reactive), oxide_acidity_basicity(basic), reaction_with_oxygen(forms_oxides), reaction_with_water(vigorous), reaction_with_acids(produces_hydrogen), magnetism(paramagnetic)]).
-miscellaneous_properties(lithium, [flame_test_color(red), isotope_count(2), allotrope_count(0), corrosion_resistance(low_resistance), refractive_index(N/A), hardness(0.6), radioactivity(no), group(1), period(2), ionization_energy(5.392), electron_configuration('1s22s1'), electronegativity(0.912)]).
+miscellaneous_properties(lithium, [flame_test_color(red), isotope_count(2), allotrope_count(1), corrosion_resistance(low_resistance), refractive_index(N/A), hardness(0.6), radioactivity(no), group(1), period(2), ionization_energy(5.392), electron_configuration('1s22s1'), electronegativity(0.912)]).
+
+% Properties for: Beryllium
+atomic_properties(beryllium, atomic_number(4), atomic_symbol(be), atomic_mass(9.01)).
+oxidation_states(beryllium, [+2]).
+physical_properties(beryllium, [state(solid), color([silvery_white]), density(1.85), melting_point(1287), boiling_point(2471), thermal_conductivity(high), electrical_conductivity(high), crystal_structure(hcp)]).
+chemical_properties(beryllium, [toxicity(yes), reactivity(highly_reactive), oxide_acidity_basicity(basic), reaction_with_oxygen(forms_oxides), reaction_with_water(no_reaction), reaction_with_acids(produces_hydrogen), magnetism(diamagnetic)]).
+miscellaneous_properties(beryllium, [flame_test_color(n/a), isotope_count(12), allotrope_count(2), corrosion_resistance(protective_oxide_layer), refractive_index(N/A), hardness(6), radioactivity(no), group(2), period(2), ionization_energy(9.323), electron_configuration('1s22s2'), electronegativity(1.576)]).
+
+% Properties for: Boron
+atomic_properties(boron, atomic_number(5), atomic_symbol(b), atomic_mass(10.81)).
+oxidation_states(boron, [+3]).
+physical_properties(boron, [state(solid), color([brown]), density(2.37), melting_point(2076), boiling_point(3927), thermal_conductivity(low_(insulator)), electrical_conductivity(low_(insulator)), crystal_structure(rhombohedral)]).
+chemical_properties(boron, [toxicity(no), reactivity(moderately_reactive), oxide_acidity_basicity(acidic), reaction_with_oxygen(forms_oxides), reaction_with_water(no_reaction), reaction_with_acids(no_reaction), magnetism(diamagnetic)]).
+miscellaneous_properties(boron, [flame_test_color(bright_green), isotope_count(2), allotrope_count(4), corrosion_resistance(low_general_reactivity), refractive_index(N/A), hardness(9.3), radioactivity(no), group(13), period(1), ionization_energy(8.298), electron_configuration('1s22s22p1'), electronegativity(2.051)]).
+
+% Properties for: Carbon
+atomic_properties(carbon, atomic_number(6), atomic_symbol(c), atomic_mass(12.01)).
+oxidation_states(carbon, [+4, +2, -4]).
+physical_properties(carbon, [state(solid), color([black,clear,gray]), density(2.267), melting_point(3550), boiling_point(3825), thermal_conductivity(high), electrical_conductivity(high), crystal_structure(hp4)]).
+chemical_properties(carbon, [toxicity(no), reactivity(highly_reactive), oxide_acidity_basicity(acidic), reaction_with_oxygen(forms_oxides), reaction_with_water(no_reaction), reaction_with_acids(no_reaction), magnetism(diamagnetic)]).
+miscellaneous_properties(carbon, [flame_test_color(orange), isotope_count(3), allotrope_count(500), corrosion_resistance(n/a), refractive_index(N/A), hardness(10), radioactivity(no), group(14), period(2), ionization_energy(11.26), electron_configuration('1s22s22p2'), electronegativity(2.544)]).
+
+% Properties for: Nitrogen
+atomic_properties(nitrogen, atomic_number(7), atomic_symbol(n), atomic_mass(14.01)).
+oxidation_states(nitrogen, [+5, +4, +3, +2, +1, -1, -2, -3]).
+physical_properties(nitrogen, [state(gas), color([colorless]), density(0.0012506), melting_point(-210), boiling_point(-195.795), thermal_conductivity(low_(insulator)), electrical_conductivity(low_(insulator)), crystal_structure(hp4)]).
+chemical_properties(nitrogen, [toxicity(no), reactivity(inert), oxide_acidity_basicity(acidic), reaction_with_oxygen(forms_oxides), reaction_with_water(no_reaction), reaction_with_acids(no_reaction), magnetism(diamagnetic)]).
+miscellaneous_properties(nitrogen, [flame_test_color(blue), isotope_count(15), allotrope_count(1), corrosion_resistance(n/a), refractive_index(1), hardness(n/a), radioactivity(no), group(15), period(2), ionization_energy(14.534), electron_configuration('1s22s22p3'), electronegativity(3.066)]).
+
+% Properties for: Oxygen
+atomic_properties(oxygen, atomic_number(8), atomic_symbol(o), atomic_mass(16.00)).
+oxidation_states(oxygen, [-2]).
+physical_properties(oxygen, [state(gas), color([colorless]), density(0.001429), melting_point(-218.79), boiling_point(-182.95), thermal_conductivity(low_(insulator)), electrical_conductivity(low_(insulator)), crystal_structure(cubic)]).
+chemical_properties(oxygen, [toxicity(no), reactivity(highly_reactive), oxide_acidity_basicity(not_applicable), reaction_with_oxygen(not_applicable), reaction_with_water(no_reaction), reaction_with_acids(no_reaction), magnetism(paramagnetic)]).
+miscellaneous_properties(oxygen, [flame_test_color(n/a), isotope_count(3), allotrope_count(3), corrosion_resistance(n/a), refractive_index(1), hardness(n/a), radioactivity(no), group(16), period(2), ionization_energy(13.618), electron_configuration('1s22s22p4'), electronegativity(3.61)]).
+
+% Properties for: Fluorine
+atomic_properties(fluorine, atomic_number(9), atomic_symbol(f), atomic_mass(19.00)).
+oxidation_states(fluorine, [-1]).
+physical_properties(fluorine, [state(gas), color([pale_yellow]), density(0.001696), melting_point(-219.62), boiling_point(-188.12), thermal_conductivity(low_(insulator)), electrical_conductivity(low_(insulator)), crystal_structure(cubic)]).
+chemical_properties(fluorine, [toxicity(yes), reactivity(highly_reactive), oxide_acidity_basicity(acidic), reaction_with_oxygen(forms_oxides), reaction_with_water(vigorous), reaction_with_acids(no_reaction), magnetism(diamagnetic)]).
+miscellaneous_properties(fluorine, [flame_test_color(n/a), isotope_count(1), allotrope_count(2), corrosion_resistance(n/a), refractive_index(1), hardness(n/a), radioactivity(no), group(17), period(2), ionization_energy(17.423), electron_configuration('1s22s22p5'), electronegativity(4.193)]).
+
+% Properties for: Neon
+atomic_properties(neon, atomic_number(10), atomic_symbol(ne), atomic_mass(20.18)).
+oxidation_states(neon, [0]).
+physical_properties(neon, [state(gas), color([colorless]), density(0.0008999), melting_point(-248.59), boiling_point(-246.08), thermal_conductivity(low_(insulator)), electrical_conductivity(low_(insulator)), crystal_structure(fcc)]).
+chemical_properties(neon, [toxicity(no), reactivity(inert), oxide_acidity_basicity(not_applicable), reaction_with_oxygen(does_not_react), reaction_with_water(no_reaction), reaction_with_acids(no_reaction), magnetism(diamagnetic)]).
+miscellaneous_properties(neon, [flame_test_color(n/a), isotope_count(3), allotrope_count(1), corrosion_resistance(n/a), refractive_index(1), hardness(n/a), radioactivity(no), group(18), period(1), ionization_energy(21.565), electron_configuration('1s22s22p6'), electronegativity(4.787)]).
+
+% Properties for: Sodium
+atomic_properties(sodium, atomic_number(11), atomic_symbol(na), atomic_mass(22.99)).
+oxidation_states(sodium, [+1]).
+physical_properties(sodium, [state(solid), color([silvery_white]), density(0.97), melting_point(97.8), boiling_point(883), thermal_conductivity(high), electrical_conductivity(high), crystal_structure(bcc)]).
+chemical_properties(sodium, [toxicity(yes), reactivity(highly_reactive), oxide_acidity_basicity(basic), reaction_with_oxygen(forms_oxides), reaction_with_water(vigorous), reaction_with_acids(produces_hydrogen), magnetism(paramagnetic)]).
+miscellaneous_properties(sodium, [flame_test_color(yellow_orange), isotope_count(1), allotrope_count(1), corrosion_resistance(low_resistance), refractive_index(N/A), hardness(0.5), radioactivity(no), group(1), period(3), ionization_energy(5.139), electron_configuration('1s22s22p63s1'), electronegativity(0.869)]).
+
+% Properties for: Magnesium
+atomic_properties(magnesium, atomic_number(12), atomic_symbol(mg), atomic_mass(24.31)).
+oxidation_states(magnesium, [+2]).
+physical_properties(magnesium, [state(solid), color([silvery_white]), density(1.74), melting_point(650), boiling_point(1090), thermal_conductivity(high), electrical_conductivity(high), crystal_structure(hcp)]).
+chemical_properties(magnesium, [toxicity(no), reactivity(highly_reactive), oxide_acidity_basicity(basic), reaction_with_oxygen(forms_oxides), reaction_with_water(mild), reaction_with_acids(produces_hydrogen), magnetism(paramagnetic)]).
+miscellaneous_properties(magnesium, [flame_test_color(white), isotope_count(3), allotrope_count(1), corrosion_resistance(low_resistance), refractive_index(N/A), hardness(2.5), radioactivity(no), group(2), period(3), ionization_energy(7.646), electron_configuration('1s22s22p63s2'), electronegativity(1.293)]).
+
+% Properties for: Aluminum
+atomic_properties(aluminum, atomic_number(13), atomic_symbol(al), atomic_mass(26.98)).
+oxidation_states(aluminum, [+3]).
+physical_properties(aluminum, [state(solid), color([silvery_gray]), density(2.7), melting_point(660.323), boiling_point(2519), thermal_conductivity(high), electrical_conductivity(high), crystal_structure(fcc)]).
+chemical_properties(aluminum, [toxicity(yes), reactivity(highly_reactive), oxide_acidity_basicity(amphoteric), reaction_with_oxygen(forms_oxides), reaction_with_water(mild), reaction_with_acids(produces_hydrogen), magnetism(paramagnetic)]).
+miscellaneous_properties(aluminum, [flame_test_color(silver_white), isotope_count(2), allotrope_count(1), corrosion_resistance(protective_oxide_layer), refractive_index(N/A), hardness(2.75), radioactivity(no), group(13), period(3), ionization_energy(5.986), electron_configuration('1s22s22p63s23p1'), electronegativity(1.613)]).
+
+% Properties for: Silicon
+atomic_properties(silicon, atomic_number(14), atomic_symbol(si), atomic_mass(28.09)).
+oxidation_states(silicon, [+4, +2, -4]).
+physical_properties(silicon, [state(solid), color([blue_gray]), density(2.3296), melting_point(1414), boiling_point(3265), thermal_conductivity(high), electrical_conductivity(medium_(semiconductor)), crystal_structure(cf8)]).
+chemical_properties(silicon, [toxicity(no), reactivity(moderately_reactive), oxide_acidity_basicity(acidic), reaction_with_oxygen(forms_oxides), reaction_with_water(mild), reaction_with_acids(no_reaction), magnetism(diamagnetic)]).
+miscellaneous_properties(silicon, [flame_test_color(n/a), isotope_count(3), allotrope_count(5), corrosion_resistance(moderately_resistant), refractive_index(N/A), hardness(7), radioactivity(no), group(14), period(3), ionization_energy(8.152), electron_configuration('1s22s22p63s23p2'), electronegativity(1.916)]).
+
+% Properties for: Phosphorus
+atomic_properties(phosphorus, atomic_number(15), atomic_symbol(p), atomic_mass(30.97)).
+oxidation_states(phosphorus, [+5, +3, -3]).
+physical_properties(phosphorus, [state(solid), color([red,white,black]), density(1.82), melting_point(44.15), boiling_point(280.5), thermal_conductivity(low_(insulator)), electrical_conductivity(low_(insulator)), crystal_structure(n/a)]).
+chemical_properties(phosphorus, [toxicity(yes), reactivity(highly_reactive), oxide_acidity_basicity(acidic), reaction_with_oxygen(forms_oxides), reaction_with_water(mild), reaction_with_acids(no_reaction), magnetism(diamagnetic)]).
+miscellaneous_properties(phosphorus, [flame_test_color(blue_green), isotope_count(23), allotrope_count(3), corrosion_resistance(n/a), refractive_index(N/A), hardness(n/a), radioactivity(no), group(15), period(3), ionization_energy(10.487), electron_configuration('1s22s22p63s23p3'), electronegativity(2.253)]).
+
+% Properties for: Sulfur
+atomic_properties(sulfur, atomic_number(16), atomic_symbol(s), atomic_mass(32.07)).
+oxidation_states(sulfur, [+6, +4, -2]).
+physical_properties(sulfur, [state(solid), color([yellow]), density(2.067), melting_point(115.21), boiling_point(444.6), thermal_conductivity(low_(insulator)), electrical_conductivity(low_(insulator)), crystal_structure(orthorhombic)]).
+chemical_properties(sulfur, [toxicity(no), reactivity(moderately_reactive), oxide_acidity_basicity(acidic), reaction_with_oxygen(forms_oxides), reaction_with_water(mild), reaction_with_acids(no_reaction), magnetism(diamagnetic)]).
+miscellaneous_properties(sulfur, [flame_test_color(blue), isotope_count(23), allotrope_count(30), corrosion_resistance(n/a), refractive_index(N/A), hardness(2), radioactivity(no), group(16), period(3), ionization_energy(10.36), electron_configuration('1s22s22p63s23p4'), electronegativity(2.589)]).
+
+% Properties for: Chlorine
+atomic_properties(chlorine, atomic_number(17), atomic_symbol(cl), atomic_mass(35.45)).
+oxidation_states(chlorine, [+7, +5, +1, -1]).
+physical_properties(chlorine, [state(gas), color([yellow_green]), density(0.003214), melting_point(-101.5), boiling_point(-34.04), thermal_conductivity(low_(insulator)), electrical_conductivity(low_(insulator)), crystal_structure(orthorhombic)]).
+chemical_properties(chlorine, [toxicity(yes), reactivity(highly_reactive), oxide_acidity_basicity(acidic), reaction_with_oxygen(forms_oxides), reaction_with_water(mild), reaction_with_acids(no_reaction), magnetism(diamagnetic)]).
+miscellaneous_properties(chlorine, [flame_test_color(yellow_green), isotope_count(25), allotrope_count(1), corrosion_resistance(n/a), refractive_index(1.000773), hardness(n/a), radioactivity(yes), group(17), period(3), ionization_energy(12.968), electron_configuration('1s22s22p63s22p6'), electronegativity(2.869)]).
+
+% Properties for: Argon
+atomic_properties(argon, atomic_number(18), atomic_symbol(ar), atomic_mass(39.90)).
+oxidation_states(argon, [0]).
+physical_properties(argon, [state(gas), color([colorless]), density(0.0017837), melting_point(-189.35), boiling_point(-185.85), thermal_conductivity(low_(insulator)), electrical_conductivity(low_(insulator)), crystal_structure(fcc)]).
+chemical_properties(argon, [toxicity(no), reactivity(inert), oxide_acidity_basicity(not_applicable), reaction_with_oxygen(does_not_react), reaction_with_water(no_reaction), reaction_with_acids(no_reaction), magnetism(diamagnetic)]).
+miscellaneous_properties(argon, [flame_test_color(n/a), isotope_count(26), allotrope_count(1), corrosion_resistance(n/a), refractive_index(1), hardness(n/a), radioactivity(no), group(18), period(3), ionization_energy(15.760), electron_configuration('1s22s22p63s23p6'), electronegativity(3.242)]).
+
+% Properties for: Potassium
+atomic_properties(potassium, atomic_number(19), atomic_symbol(k), atomic_mass(39.10)).
+oxidation_states(potassium, [+1]).
+physical_properties(potassium, [state(solid), color([silvery_white]), density(0.89), melting_point(63.38), boiling_point(759), thermal_conductivity(high), electrical_conductivity(high), crystal_structure(bcc)]).
+chemical_properties(potassium, [toxicity(yes), reactivity(highly_reactive), oxide_acidity_basicity(basic), reaction_with_oxygen(forms_oxides), reaction_with_water(vigorous), reaction_with_acids(produces_hydrogen), magnetism(paramagnetic)]).
+miscellaneous_properties(potassium, [flame_test_color(bluish_purple), isotope_count(25), allotrope_count(1), corrosion_resistance(low_resistance), refractive_index(N/A), hardness(0.4), radioactivity(no), group(1), period(4), ionization_energy(4.341), electron_configuration('1s22s22p63s23p64s1'), electronegativity(0.734)]).
+
+% Properties for: Calcium
+atomic_properties(calcium, atomic_number(20), atomic_symbol(ca), atomic_mass(40.08)).
+oxidation_states(calcium, [+2]).
+physical_properties(calcium, [state(solid), color([silvery_white]), density(1.54), melting_point(842), boiling_point(1484), thermal_conductivity(high), electrical_conductivity(high), crystal_structure(fcc)]).
+chemical_properties(calcium, [toxicity(no), reactivity(highly_reactive), oxide_acidity_basicity(basic), reaction_with_oxygen(forms_oxides), reaction_with_water(vigorous), reaction_with_acids(produces_hydrogen), magnetism(diamagnetic)]).
+miscellaneous_properties(calcium, [flame_test_color(orange_red), isotope_count(26), allotrope_count(2), corrosion_resistance(low_resistance), refractive_index(N/A), hardness(1.75), radioactivity(no), group(2), period(4), ionization_energy(6.113), electron_configuration('1s22s22p63s23p64s2'), electronegativity(1.034)]).
+
+% Properties for: Iron
+atomic_properties(iron, atomic_number(26), atomic_symbol(fe), atomic_mass(55.84)).
+oxidation_states(iron, [+2, +3]).
+physical_properties(iron, [state(solid), color([gray]), density(7.874), melting_point(1538), boiling_point(2861), thermal_conductivity(high), electrical_conductivity(high), crystal_structure(bcc)]).
+chemical_properties(iron, [toxicity(no), reactivity(moderately_reactive), oxide_acidity_basicity(amphoteric), reaction_with_oxygen(forms_oxides), reaction_with_water(mild), reaction_with_acids(produces_hydrogen), magnetism(ferromagnetic)]).
+miscellaneous_properties(iron, [flame_test_color(orange_brown), isotope_count(32), allotrope_count(4), corrosion_resistance(low_resistance), refractive_index(N/A), hardness(4), radioactivity(no), group(1), period(4), ionization_energy(7.902), electron_configuration('1s22s22p63s23p64s23d6'), electronegativity(1.8)]).
 
