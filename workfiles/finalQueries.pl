80fa880c5e962c4a9426e4188b0d69832f43aba2
Kaleab Hailemeskel <kaleabhailemeskel47@gmail.com>
2024-12-20 12:21:59 +0300

    Now, the color and oxidation states can determine without listing all possible oxidation or color elements, as the tester may not find all values during testing. Therefore, inputting one value can suffice for the process. none functionality has been added so that if the tester doesn't know a specific element's property, it can be skipped without any confusion.

--- workfiles/finalQueries.pl
+++ workfiles/finalQueries.pl
@@ -1,43 +1,174 @@
 % Now, the color and oxidation states can be determined
-% without listing all possible oxidation or color elements,
-% as the tester may not find all values during testing. Therefore, inputting one value can suffice for the process.
-
+%   without listing all possible oxidation or color elements,
+%   as the tester may not find all values during testing. Therefore, inputting one value can suffice for the process.
+% none functionality has been added so that if the tester doesn't
+%   know a specific element's property, it can be skipped without any confusion.
 %%%%%%%%%% KNOWLEDGE BASE FROM test_knowledge_base2.pl GOES HERE %%%%%%%%%%%%
 
+% Properties for: Gold
+has_property(gold, atomicNumber, 79).
+has_property(gold, atomicMass, 196.97).
+has_property(gold, color, [yellow]).
+has_property(gold, oxidationStates, [+3, +1]).
+has_property(gold, symbol, au).
+has_property(gold, stateOfMatter, solid).
+has_property(gold, density, 19.282).
+has_property(gold, meltingPoint, 1064.18).
+has_property(gold, boilingPoint, 2856).
+has_property(gold, thermalConductivity, high).
+has_property(gold, electricalConductivity, high).
+has_property(gold, crystalStructure, fcc).
+has_property(gold, toxicity, no).
+has_property(gold, reactivity, inert).
+has_property(gold, oxideAcidityBasicity, not_applicable).
+has_property(gold, reactionWithOxygen, does_not_react).
+has_property(gold, reactionWithWater, no_reaction).
+has_property(gold, reactionWithAcids, no_reaction).
+has_property(gold, magnetism, diamagnetic).
+has_property(gold, flameTestColor, n/a).
+has_property(gold, isotopeCount, 41).
+has_property(gold, allotropeCount, 1).
+has_property(gold, corrosionResistance, low_general_reactivity).
+has_property(gold, refractiveIndex, n/a).
+has_property(gold, hardness, 2.5).
+has_property(gold, radioactivity, no).
+has_property(gold, group, 11).
+has_property(gold, period, 6).
+has_property(gold, ionizationEnergy, 9.226).
+has_property(gold, electronegativity, 1.92).
+has_property(gold, electronConfiguration, '1s22s22p63s23p63d104s24p64d104f145s25p65d106s1').
 
-% Properties for: Manganese
-has_property(manganese, atomicNumber, 25).
-has_property(manganese, atomicMass, 54.94).
-has_property(manganese, color, [silvery_gray]).
-has_property(manganese, oxidationStates, [+7, +4, +3, +2]).
-has_property(manganese, symbol, mn).
-has_property(manganese, stateOfMatter, solid).
-has_property(manganese, density, 7.3).
-has_property(manganese, meltingPoint, 1246).
-has_property(manganese, boilingPoint, 2061).
-has_property(manganese, thermalConductivity, high).
-has_property(manganese, electricalConductivity, high).
-has_property(manganese, crystalStructure, bcc).
-has_property(manganese, toxicity, no).
-has_property(manganese, reactivity, highly_reactive).
-has_property(manganese, oxideAcidityBasicity, basic).
-has_property(manganese, reactionWithOxygen, forms_oxides).
-has_property(manganese, reactionWithWater, mild).
-has_property(manganese, reactionWithAcids, produces_hydrogen).
-has_property(manganese, magnetism, paramagnetic).
-has_property(manganese, flameTestColor, yellow_green).
-has_property(manganese, isotopeCount, 28).
-has_property(manganese, allotropeCount, 4).
-has_property(manganese, corrosionResistance, protective_oxide_layer).
-has_property(manganese, refractiveIndex, n/a).
-has_property(manganese, hardness, 6).
-has_property(manganese, radioactivity, no).
-has_property(manganese, group, 7).
-has_property(manganese, period, 4).
-has_property(manganese, ionizationEnergy, 7.434).
-has_property(manganese, electronegativity, 1.75).
-has_property(manganese, electronConfiguration, '1s22s22p63s23p64s23d5').
+% Properties for: Oxygen
+has_property(oxygen, atomicNumber, 8).
+has_property(oxygen, atomicMass, 16.00).
+has_property(oxygen, color, [colorless]).
+has_property(oxygen, oxidationStates, [-2]).
+has_property(oxygen, symbol, o).
+has_property(oxygen, stateOfMatter, gas).
+has_property(oxygen, density, 0.001429).
+has_property(oxygen, meltingPoint, -218.79).
+has_property(oxygen, boilingPoint, -182.95).
+has_property(oxygen, thermalConductivity, low_(insulator)).
+has_property(oxygen, electricalConductivity, low_(insulator)).
+has_property(oxygen, crystalStructure, cubic).
+has_property(oxygen, toxicity, no).
+has_property(oxygen, reactivity, highly_reactive).
+has_property(oxygen, oxideAcidityBasicity, not_applicable).
+has_property(oxygen, reactionWithOxygen, not_applicable).
+has_property(oxygen, reactionWithWater, no_reaction).
+has_property(oxygen, reactionWithAcids, no_reaction).
+has_property(oxygen, magnetism, paramagnetic).
+has_property(oxygen, flameTestColor, n/a).
+has_property(oxygen, isotopeCount, 3).
+has_property(oxygen, allotropeCount, 3).
+has_property(oxygen, corrosionResistance, n/a).
+has_property(oxygen, refractiveIndex, 1).
+has_property(oxygen, hardness, n/a).
+has_property(oxygen, radioactivity, no).
+has_property(oxygen, group, 16).
+has_property(oxygen, period, 2).
+has_property(oxygen, ionizationEnergy, 13.618).
+has_property(oxygen, electronegativity, 3.61).
+has_property(oxygen, electronConfiguration, '1s22s22p4').
 
+% Properties for: Chlorine
+has_property(chlorine, atomicNumber, 17).
+has_property(chlorine, atomicMass, 35.45).
+has_property(chlorine, color, [yellow_green]).
+has_property(chlorine, oxidationStates, [+7, +5, +1, -1]).
+has_property(chlorine, symbol, cl).
+has_property(chlorine, stateOfMatter, gas).
+has_property(chlorine, density, 0.003214).
+has_property(chlorine, meltingPoint, -101.5).
+has_property(chlorine, boilingPoint, -34.04).
+has_property(chlorine, thermalConductivity, low_(insulator)).
+has_property(chlorine, electricalConductivity, low_(insulator)).
+has_property(chlorine, crystalStructure, orthorhombic).
+has_property(chlorine, toxicity, yes).
+has_property(chlorine, reactivity, highly_reactive).
+has_property(chlorine, oxideAcidityBasicity, acidic).
+has_property(chlorine, reactionWithOxygen, forms_oxides).
+has_property(chlorine, reactionWithWater, mild).
+has_property(chlorine, reactionWithAcids, no_reaction).
+has_property(chlorine, magnetism, diamagnetic).
+has_property(chlorine, flameTestColor, yellow_green).
+has_property(chlorine, isotopeCount, 25).
+has_property(chlorine, allotropeCount, 1).
+has_property(chlorine, corrosionResistance, n/a).
+has_property(chlorine, refractiveIndex, 1.000773).
+has_property(chlorine, hardness, n/a).
+has_property(chlorine, radioactivity, no).
+has_property(chlorine, group, 17).
+has_property(chlorine, period, 3).
+has_property(chlorine, ionizationEnergy, 12.968).
+has_property(chlorine, electronegativity, 2.869).
+has_property(chlorine, electronConfiguration, '1s22s22p63s22p6').
+
+% Properties for: Nitrogen
+has_property(nitrogen, atomicNumber, 7).
+has_property(nitrogen, atomicMass, 14.01).
+has_property(nitrogen, color, [colorless]).
+has_property(nitrogen, oxidationStates, [+5, +4, +3, +2, +1, -1, -2, -3]).
+has_property(nitrogen, symbol, n).
+has_property(nitrogen, stateOfMatter, gas).
+has_property(nitrogen, density, 0.0012506).
+has_property(nitrogen, meltingPoint, -210).
+has_property(nitrogen, boilingPoint, -195.795).
+has_property(nitrogen, thermalConductivity, low_(insulator)).
+has_property(nitrogen, electricalConductivity, low_(insulator)).
+has_property(nitrogen, crystalStructure, hp4).
+has_property(nitrogen, toxicity, no).
+has_property(nitrogen, reactivity, inert).
+has_property(nitrogen, oxideAcidityBasicity, acidic).
+has_property(nitrogen, reactionWithOxygen, forms_oxides).
+has_property(nitrogen, reactionWithWater, no_reaction).
+has_property(nitrogen, reactionWithAcids, no_reaction).
+has_property(nitrogen, magnetism, diamagnetic).
+has_property(nitrogen, flameTestColor, blue).
+has_property(nitrogen, isotopeCount, 15).
+has_property(nitrogen, allotropeCount, 1).
+has_property(nitrogen, corrosionResistance, n/a).
+has_property(nitrogen, refractiveIndex, 1).
+has_property(nitrogen, hardness, n/a).
+has_property(nitrogen, radioactivity, no).
+has_property(nitrogen, group, 15).
+has_property(nitrogen, period, 2).
+has_property(nitrogen, ionizationEnergy, 14.534).
+has_property(nitrogen, electronegativity, 3.066).
+has_property(nitrogen, electronConfiguration, '1s22s22p3').
+
+% Properties for: Carbon
+has_property(carbon, atomicNumber, 6).
+has_property(carbon, atomicMass, 12.01).
+has_property(carbon, color, [black,clear,gray]).
+has_property(carbon, oxidationStates, [+4, +2, -4]).
+has_property(carbon, symbol, c).
+has_property(carbon, stateOfMatter, solid).
+has_property(carbon, density, 2.267).
+has_property(carbon, meltingPoint, 3550).
+has_property(carbon, boilingPoint, 3825).
+has_property(carbon, thermalConductivity, high).
+has_property(carbon, electricalConductivity, high).
+has_property(carbon, crystalStructure, hp4).
+has_property(carbon, toxicity, no).
+has_property(carbon, reactivity, highly_reactive).
+has_property(carbon, oxideAcidityBasicity, acidic).
+has_property(carbon, reactionWithOxygen, forms_oxides).
+has_property(carbon, reactionWithWater, no_reaction).
+has_property(carbon, reactionWithAcids, no_reaction).
+has_property(carbon, magnetism, diamagnetic).
+has_property(carbon, flameTestColor, orange).
+has_property(carbon, isotopeCount, 3).
+has_property(carbon, allotropeCount, 500).
+has_property(carbon, corrosionResistance, n/a).
+has_property(carbon, refractiveIndex, 2.417).
+has_property(carbon, hardness, 10).
+has_property(carbon, radioactivity, no).
+has_property(carbon, group, 14).
+has_property(carbon, period, 2).
+has_property(carbon, ionizationEnergy, 11.26).
+has_property(carbon, electronegativity, 2.544).
+has_property(carbon, electronConfiguration, '1s22s22p2').
 
 %%%%%%%%%%%%%%%%% RULE DEFINITION AND SUCH %%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%
@@ -95,11 +226,19 @@ proceed(yes, ask_general_questions, PossElement):-
     write('What is the Period Number of the Element? '), read(PeriodNumber),
     % Computation would start here taking the Previous ElementAnswer in consideration
 
-   findall(Element, (has_property(Element, group,GroupNumber),
-         has_property(Element, period, PeriodNumber),has_property(Element, symbol, Symbol),
-       has_property(Element, atomicNumber, AtomicNumber)), PossElement),
-       (PossElement == [] -> !,fail;
-            write('The Element is '), write(PossElement), nl).
+    findall(Element, (
+            (GroupNumber == 'none' ; has_property(Element, group, GroupNumber)),
+            (PeriodNumber == 'none' ; has_property(Element, period, PeriodNumber)),
+            (Symbol == 'none' ; has_property(Element, symbol, Symbol)),
+            (AtomicNumber == 'none' ; has_property(Element, atomicNumber, AtomicNumber))
+        ), PossElement),
+
+    (PossElement == [] -> !, fail;
+        write('The Element is '), write(PossElement), nl).
+
+
+
+
 
 proceed(yes, ask_physical_properties, PossElement):-
 
@@ -113,18 +252,22 @@ proceed(yes, ask_physical_properties, PossElement):-
     write('What is the Electrical Conductivity of the Element? '), read(ElectricalConductivity),
     write('What is the Crystal Structure of the Element? '), read(CrystalStructure),
     % Computation would start here taking the Previous ElementAnswer in consideration
-    findall(Element, (    has_property(Element, atomicMass, MassNumber),
-                          has_property(Element, stateOfMatter, StateOfMatter),
-                          has_property(Element, color, ElementColors),
-                          check_all_membership(Colors, ElementColors),
-                          has_property(Element, density, Density),
-                          has_property(Element, meltingPoint, MeltingPoint),
-                          has_property(Element, boilingPoint, BoilingPoint),
-                          has_property(Element, thermalConductivity, ThermalConductivity),
-                          has_property(Element, electricalConductivity, ElectricalConductivity),
-                          has_property(Element, crystalStructure,CrystalStructure)), PossElement),
-    (PossElement == [] -> !,fail;
-                write('The Element is '), write(PossElement), nl).
+   findall(Element, (
+       (MassNumber == 'none' ; has_property(Element, atomicMass, MassNumber)),
+       (StateOfMatter == 'none' ; has_property(Element, stateOfMatter, StateOfMatter)),
+       (Colors == 'none' ; has_property(Element, color, Colors)),
+       (Colors == 'none' ; check_all_membership(Colors, Colors)),
+       (Density == 'none' ; has_property(Element, density, Density)),
+       (MeltingPoint == 'none' ; has_property(Element, meltingPoint, MeltingPoint)),
+       (BoilingPoint == 'none' ; has_property(Element, boilingPoint, BoilingPoint)),
+       (ThermalConductivity == 'none' ; has_property(Element, thermalConductivity, ThermalConductivity)),
+       (ElectricalConductivity == 'none' ; has_property(Element, electricalConductivity, ElectricalConductivity)),
+       (CrystalStructure == 'none' ; has_property(Element, crystalStructure, CrystalStructure))
+   ), PossElement),
+
+   (PossElement == [] -> !, fail;
+       write('The Element is '), write(PossElement), nl).
+
 
 
 
@@ -140,18 +283,21 @@ proceed(yes, ask_chemical_properties, PossElement) :-
     write('What is the ionization energy of the element? '), read(IonizationEnergy),
     write('How many allotropes does the element have? '), read(AllotropeCount),
     % Computation would start here taking the Previous ElementAnswer in consideration
-    findall(Element, (has_property(Element,reactivity,Reactivity ),
-    has_property(Element, oxideAcidityBasicity ,OxideAcidityBasicity),
-    has_property(Element, reactionWithWater,ReactionWithWater),
-    has_property(Element, reactionWithAcids,ReactionWithAcids),
-    has_property(Element, reactionWithOxygen,ReactionWithOxygen),
-    has_property(Element, oxidationStates,AllOxidationStates),
-    check_all_membership(OxidationStates, AllOxidationStates),
-    has_property(Element, electronegativity ,Electronegativity),
-    has_property(Element, ionizationEnergy,IonizationEnergy),
-    has_property(Element,allotropeCount ,AllotropeCount)), PossElement),
-    (PossElement == [] -> !,fail;
-                write('The Element is '), write(PossElement), nl).
+    findall(Element, (
+        (Reactivity == 'none' ; has_property(Element, reactivity, Reactivity)),
+        (OxideAcidityBasicity == 'none' ; has_property(Element, oxideAcidityBasicity, OxideAcidityBasicity)),
+        (ReactionWithWater == 'none' ; has_property(Element, reactionWithWater, ReactionWithWater)),
+        (ReactionWithAcids == 'none' ; has_property(Element, reactionWithAcids, ReactionWithAcids)),
+        (ReactionWithOxygen == 'none' ; has_property(Element, reactionWithOxygen, ReactionWithOxygen)),
+        (OxidationStates == 'none' ; has_property(Element, oxidationStates, AllOxidationStates)),
+        (OxidationStates == 'none' ; check_all_membership(OxidationStates, AllOxidationStates)),
+        (Electronegativity == 'none' ; has_property(Element, electronegativity, Electronegativity)),
+        (IonizationEnergy == 'none' ; has_property(Element, ionizationEnergy, IonizationEnergy)),
+        (AllotropeCount == 'none' ; has_property(Element, allotropeCount, AllotropeCount))
+    ), PossElement),
+
+    (PossElement == [] -> !, fail;
+        write('The Element is '), write(PossElement), nl).
 
 proceed(yes, ask_special_properties, PossElement) :-
     write('Is the element radioactive? (yes/no) '), read(Radioactivity),
@@ -162,11 +308,15 @@ proceed(yes, ask_special_properties, PossElement) :-
     write('What is the hardness of the element on the Mohs scale? '), read(Hardness),
     % Computation would start here taking the Previous ElementAnswer in consideration
 
-    findall(Element, (has_property(Element, radioactivity, Radioactivity),
-    has_property(Element, isotopeCount, IsotopeCount),
-    has_property(Element, flameTestColor, FlameTestColor),
-    has_property(Element, toxicity,Toxicity),
-    has_property(Element, corrosionResistance, CorrosionResistance),
-    has_property(Element, hardness, Hardness)), PossElement),
-    (PossElement == [] -> !,fail;
-                    write('The Element is '), write(PossElement), nl).
+    findall(Element, (
+        (Radioactivity == 'none' ; has_property(Element, radioactivity, Radioactivity)),
+        (IsotopeCount == 'none' ; has_property(Element, isotopeCount, IsotopeCount)),
+        (FlameTestColor == 'none' ; has_property(Element, flameTestColor, FlameTestColor)),
+        (Toxicity == 'none' ; has_property(Element, toxicity, Toxicity)),
+        (CorrosionResistance == 'none' ; has_property(Element, corrosionResistance, CorrosionResistance)),
+        (Hardness == 'none' ; has_property(Element, hardness, Hardness))
+    ), PossElement),
+
+    (PossElement == [] -> !, fail;
+        write('The Element is '), write(PossElement), nl).
+
